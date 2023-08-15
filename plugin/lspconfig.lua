2f70a6433b8d40b18e504e9b821f799b770e7081
naoto67 <uver.com67@gmail.com>
2023-08-15 23:38:11 +0900

    fix lsp keymap

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -1,26 +1,25 @@
 local on_attach = function(client, bufnr)
+	-- LSPが持つフォーマット機能を無効化する
+	-- →例えばtsserverはデフォルトでフォーマット機能を提供しますが、利用したくない場合はコメントアウトを解除してください
+	--client.server_capabilities.documentFormattingProvider = false
 
-  -- LSPが持つフォーマット機能を無効化する
-  -- →例えばtsserverはデフォルトでフォーマット機能を提供しますが、利用したくない場合はコメントアウトを解除してください
-  --client.server_capabilities.documentFormattingProvider = false
-
-  -- 下記ではデフォルトのキーバインドを設定しています
-  -- ほかのLSPプラグインを使う場合（例：Lspsaga）は必要ないこともあります
-
-  local set = vim.keymap.set
-  set("n", "vgd", "<cmd>:vsplit <CR> <cmd>lua vim.lsp.buf.definition()<CR>")
-  set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-  set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-  set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-  set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-  set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-  set("n", "ma", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-  set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-  set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-  set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
-  set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
-  set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
-
+	-- 下記ではデフォルトのキーバインドを設定しています
+	-- ほかのLSPプラグインを使う場合（例：Lspsaga）は必要ないこともあります
+	--
+	local set = vim.keymap.set
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	set("n", "vgd", "<cmd>:vsplit <CR> <cmd>lua vim.lsp.buf.definition()<CR>", bufopts)
+	set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", bufopts)
+	set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", bufopts)
+	set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", bufopts)
+	set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>", bufopts)
+	set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>", bufopts)
+	set("n", "ma", "<cmd>lua vim.lsp.buf.code_action()<CR>", bufopts)
+	set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", bufopts)
+	set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", bufopts)
+	-- set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", bufopts)
+	set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", bufopts)
+	set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", bufopts)
 end
 
 -- 補完プラグインであるcmp_nvim_lspをLSPと連携させています（後述）
@@ -35,10 +34,10 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
-  function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {
-      on_attach = on_attach, --keyバインドなどの設定を登録
-      capabilities = capabilities, --cmpを連携
-    }
-  end,
+	function(server_name)            -- default handler (optional)
+		require("lspconfig")[server_name].setup {
+			on_attach = on_attach,       --keyバインドなどの設定を登録
+			capabilities = capabilities, --cmpを連携
+		}
+	end,
 }
