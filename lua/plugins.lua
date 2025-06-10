8ab853eeb147bc7331102f4479f35ea2435fc8b4
ito-naoto-ab <ito_naoto@applibot.co.jp>
2025-06-10 14:22:52 +0900

    update: 使ってない copilot

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -228,6 +228,53 @@ return {
 		end,
 	},
 
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		config = function()
+			local select = require("CopilotChat.select")
+
+			require("CopilotChat").setup({
+				debug = true, -- Enable debugging
+
+				-- プロンプトの設定
+				-- デフォルトは英語なので日本語でオーバーライドしています
+				prompts = {
+					Explain = {
+						prompt = "/COPILOT_EXPLAIN カーソル上のコードの説明を段落をつけて書いてください。",
+					},
+					Tests = {
+						prompt = "/COPILOT_TESTS カーソル上のコードの詳細な単体テスト関数を書いてください。",
+					},
+					Fix = {
+						prompt = "/COPILOT_FIX このコードには問題があります。バグを修正したコードに書き換えてください。",
+					},
+					Optimize = {
+						prompt = "/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。",
+					},
+					Docs = {
+						prompt = "/COPILOT_REFACTOR 選択したコードのドキュメントを書いてください。ドキュメントをコメントとして追加した元のコードを含むコードブロックで回答してください。使用するプログラミング言語に最も適したドキュメントスタイルを使用してください（例：JavaScriptのJSDoc、Pythonのdocstringsなど）",
+					},
+					FixDiagnostic = {
+						prompt = "ファイル内の次のような診断上の問題を解決してください：",
+						selection = select.diagnostics,
+					},
+					ExTests = {
+						prompt = "/COPILOT_TESTS カーソル上のコードの詳細な単体テスト関数をテーブル駆動で書いてください。",
+					},
+				},
+			})
+		end,
+	},
+
 	-- code highlight
 	{
 		"nvim-treesitter/nvim-treesitter",
